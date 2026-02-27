1dfe6a0e1839dbae42c047a8a9a044da03f5dfb3
gdaffa <gibrandaffa92@gmail.com>
2026-02-27 16:00:28 +0700

    Update the Installation Instructions for Debian (#1838)
    * docs(download): Remove `apt-transport-https` from Debian installation
    
    * docs(download): Update Debian source list to `deb822` format

--- include/download-instructions/linux-debian-cli-community.php
+++ include/download-instructions/linux-debian-cli-community.php
@@ -4,10 +4,16 @@ On the command line, run the following commands:
 <pre><code class="language-bash line-numbers">
 # Add the packages.sury.org/php repository.
 sudo apt-get update
-sudo apt-get install -y lsb-release ca-certificates apt-transport-https curl
+sudo apt-get install -y lsb-release ca-certificates curl
 sudo curl -sSLo /tmp/debsuryorg-archive-keyring.deb https://packages.sury.org/debsuryorg-archive-keyring.deb
 sudo dpkg -i /tmp/debsuryorg-archive-keyring.deb
-sudo sh -c 'echo "deb [signed-by=/usr/share/keyrings/debsuryorg-archive-keyring.gpg] https://packages.sury.org/php/ $(lsb_release -sc) main" > /etc/apt/sources.list.d/php.list'
+sudo tee /etc/apt/sources.list.d/php.sources &lt;&lt;EOF
+Types: deb
+URIs: https://packages.sury.org/php/
+Suites: $(lsb_release -sc)
+Components: main
+Signed-By: /usr/share/keyrings/debsuryorg-archive-keyring.gpg
+EOF
 sudo apt-get update
 
 # Install PHP.
